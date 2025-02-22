6b852d90bb845b8873946c40da45c5df06157479
joospis <jasper.bushey@gmail.com>
2025-02-22 18:03:44 -0500

    Prevent good robots from scraping

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
         <meta name="theme-color" content="skyblue"/>
+        <meta name="robots" content="noindex, nofollow"/>
       </head>
       <body>
         {children}
